b6424eebbeadcd6bd9ceeae7e391e4c62c094352
DE-YU <delsin_yu@qq.com>
2026-03-02 03:14:45 +0800

    feat(demo): update usage sample with interactive layout and buttons

--- IMTUI.Net.Usage/Program.cs
+++ IMTUI.Net.Usage/Program.cs
@@ -3,20 +3,82 @@ using IMTUI.Nodes;
 
 var ui = new MyUI();
 ImmediateModeUIRenderer.Run(ui);
-Console.ReadKey();
-return;
 
 internal class MyUI : IImmediateModeTerminalUI
 {
+    private int _counter = 0;
+    private int _counter2 = 0;
+
     public void OnDraw(TerminalUIInstance tui)
     {
-        using var _ = tui.VBox();
-        tui.Label("Lorem ipsum dolor sit amet sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.", ConsoleColor.Cyan);
-        tui.Label("Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ", ConsoleColor.Yellow);
-        tui.Label("Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur. Ut enim ad minima veniam, quis nostrum exercitationem");
-        tui.Label("ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?  ", ConsoleColor.Red);
-        tui.Label("Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.  ", ConsoleColor.Blue);
+        using (tui.VBox(separation: 0, sizeFlagsVertical: ControlSizeFlags.ExpandFill))
+        {
+            using (tui.HBox(sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+            {
+                if (tui.Button($"Pressed: {_counter} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter++;
+                }
+
+                if (tui.Button($"Pressed: {_counter2} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter2++;
+                }
+            }
+
+            tui.Label("Lorem ipsum dolor sit amet sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit asper");
+
+            using (tui.HBox(sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+            {
+                if (tui.Button($"Pressed: {_counter} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter++;
+                }
+
+                if (tui.Button($"Pressed: {_counter2} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter2++;
+                }
+            }
+
+            tui.ColorRect(
+                BoxRenderingStyle.DoubleWireFrame,
+                sizeFlagsHorizontal: ControlSizeFlags.ExpandFill,
+                sizeFlagsVertical: ControlSizeFlags.ExpandFill);
+            tui.Label("Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?");
+
+            using (tui.HBox(sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+            {
+                if (tui.Button($"Pressed: {_counter} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter++;
+                }
+
+                if (tui.Button($"Pressed: {_counter2} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter2++;
+                }
+            }
+
+            tui.ColorRect(
+                BoxRenderingStyle.ThinWireFrame,
+                sizeFlagsHorizontal: ControlSizeFlags.ExpandFill,
+                sizeFlagsVertical: ControlSizeFlags.ExpandFill);
+
+            using (tui.HBox(sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+            {
+                if (tui.Button($"Pressed: {_counter} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter++;
+                }
+
+                if (tui.Button($"Pressed: {_counter2} time(s)", sizeFlagsHorizontal: ControlSizeFlags.ExpandFill))
+                {
+                    _counter2++;
+                }
+            }
+        }
+
+        // tui.Finish(false);
     }
 }
-
-
